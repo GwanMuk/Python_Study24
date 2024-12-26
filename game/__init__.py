8b3288be8ef4a188f0f64261a86dbbd71a337df5
312-03\mkm1022 <mkm1022@naver.com>
2024-12-26 10:06:59 +0900

    python_pakage연습(class모듈__init__)

---
+++ game/__init__.py
@@ -0,0 +1,15 @@
+from .graphic.render import render_test
+
+
+VERSION = 3.11
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+    print(" 게임 제작자 : 맹관묵")
+    print(" 게임 제작자 이메일 : where i am@what.com")
+    print(" 게임 제작 의뢰 : 010-1112-2223")
+
+
+# 패키지 실행 초기화 문구
+print("Initializing game....")
+print("Loading.....")
